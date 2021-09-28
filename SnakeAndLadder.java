0ea635bea634c29f82ffdaed1171d3bf8c9c4602
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-28 22:21:57 +0530

    Success : 2 Players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,77 +1,271 @@
 import java.util.Random;
 
 public class SnakeAndLadder {
-    //constant
-    final static int WIN_POSITION = 100;
-    //variables
-    int position = 0;
-    int positionCheck = 0;
-    int noOfDiceRoll = 0;
 
-    /* This main method is used to call rollDice
-     * Store the output of rollDice method
-     */
-    public static void main(String[] args) {
-        SnakeAndLadder snakeLadder = new SnakeAndLadder();
-        snakeLadder.playerOption();
-    }
-
-    /* This method is used to define constructor
-     * generate random numbers between 1 and 6
-     * return int This returns the random numbers
-     */
-    public int rollDice() {
-        Random random = new Random();
-        int dice = (random.nextInt(6) + 1);
-        System.out.println("Die Roll Is : " + dice);
-        return dice;
-    }
-
-    /* This method is used to check for no play, snake or ladder and reaches winning position 100
-     * while loop to check if player reached winning position
-     * create instance of random class
-     * initializing dice to rollDice
-     * the variable noOfDiceRoll will record the number times to dice rolled
-     * using random function to create three random options 0, 1 and 2
-     * print option 0, 1 or 2
-     * assign them to no play, ladder and snake
-     * if Ladder, Player moves forward with dice roll number
-     * if Snake, Player moves backward with dice roll number
-     * make sure not finish game till player reaches at exact position
-     * Player will start again if start position is negative or zero
-     * at last print the Players position and winning message
-     */
-    public void playerOption() {
-        while (position < WIN_POSITION) {
-            Random random = new Random();
-            int dice = rollDice();
-            noOfDiceRoll++;
-            int option = random.nextInt(3);
-            System.out.println("Player Option Is : " + option);
-            System.out.println("0.No Play 1.Ladder 2.Snake");
-            switch (option) {
-                case 1:
-                    if ((position + dice) > WIN_POSITION) {
-                        position = WIN_POSITION;
-                    } else {
-                        position += dice;
-                    }
-                    System.out.println("Ladder Is At : " + position);
-                    break;
-                case 2:
-                    position -= dice;
-                    if ((position - dice) < positionCheck) {
-                        position = positionCheck;
-                    }
-                    System.out.println("Snake Is At : " + position);
-                    break;
-                default:
-                    System.out.println("No Play : " + position);
-                    break;
-            }
-        }
-        System.out.println("Winning Position Is : " + position);
-        System.out.println("Number Of Times The Dice Was Rolled To Win The Game Is : " + noOfDiceRoll);
-    }
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int p1position1=0;
+		 int p2position1=0;
+	     System.out.println("Player1 is initially at " + p1position1);
+	     System.out.println("Player1 is initially at " + p2position1);
+	     double dice;
+	     int dice_countp1=0;
+	     int dice_countp2=0;
+	     int value;
+	     while(p1position1<=100 || p2position1<=100)
+	     {
+	     dice=Math.floor(Math.random()*10)%7;
+	     if(dice>0)
+	     {
+	     value=(int)dice;
+	     System.out.println("Dice for Player 1 rolled at "+ value);
+	     p1position1=p1position1+value;
+	     dice_countp1++;
+	     }
+	     else
+	    	 continue;
+	     dice=Math.floor(Math.random()*10)%7;
+	     if(dice>0)
+	     {
+	     value=(int)dice;
+	     System.out.println("Dice for Player 2 rolled at "+ value);
+	     p2position1=p2position1+value;
+	     dice_countp2++;
+	     }
+	    
+	     if(p1position1==2)
+	     {
+	    	 System.out.println("Player1 Ladder to 23");
+	    	 p1position1=23;
+	     }
+	     else if(p1position1==7)
+	     {
+	    	 System.out.println("Player1 Ladder to 29");
+	    	 p1position1=29;
+	     }
+	     else if(p1position1==22)
+	     {
+	    	 System.out.println("Player1 Ladder to 41");
+	    	 p1position1=41;
+	     }
+	     else if(p1position1==28)
+	     {
+	    	 System.out.println("Player1 Ladder to 77");
+	    	 p1position1=77;
+	     }
+	     else if(p1position1==30)
+	     {
+	    	 System.out.println("Player1 Ladder to 32");
+	    	 p1position1=32;
+	     }
+	     else if(p1position1==54)
+	     {
+	    	 System.out.println("Player1 Ladder to 69");
+	    	 p1position1=77;
+	     }
+	     else if(p1position1==70)
+	     {
+	    	 System.out.println("Player1 Ladder to 90");
+	    	 p1position1=90;
+	     }
+	     else if(p1position1==80)
+	     {
+	    	 System.out.println("Player1 Ladder to 98");
+	    	 p1position1=98;
+	     }
+	     else if(p1position1==27)
+	     {
+	    	 System.out.println("Player1 Snaked to 6");
+	    	 p1position1=6;
+	     }
+	     else if(p1position1==35)
+	     {
+	    	 System.out.println("Player1 Snaked to 5");
+	    	 p1position1=5;
+	     }
+	     else if(p1position1==39)
+	     {
+	    	 System.out.println("Player1 Snaked to 3");
+	    	 p1position1=3;
+	     }
+	     else if(p1position1==50)
+	     {
+	    	 System.out.println("Player1 Snaked to 34");
+	    	 p1position1=34;
+	     }
+	     else if(p1position1==59)
+	     {
+	    	 System.out.println("Player1 Snaked to 46");
+	    	 p1position1=46;
+	     }
+	     else if(p1position1==66)
+	     {
+	    	 System.out.println("Player1 Snaked to 24");
+	    	 p1position1=24;
+	     }
+	     else if(p1position1==73)
+	     {
+	    	 System.out.println("Player1 Snaked to 12");
+	    	 p1position1=12;
+	     }
+	     else if(p1position1==76)
+	     {
+	    	 System.out.println("Player1 Snaked to 63");
+	    	 p1position1=63;
+	     }
+	     else if(p1position1==89)
+	     {
+	    	 System.out.println("Player1 Snaked to 67");
+	    	 p1position1=67;
+	     }
+	     else if(p1position1==97)
+	     {
+	    	 System.out.println("Player1 Snaked to 75");
+	    	 p1position1=75;
+	     }
+	     else if(p1position1==99)
+	     {
+	    	 System.out.println("Player1 Snaked to 26");
+	    	 p1position1=26;
+	     }
+	     
+	     if(p2position1==2)
+	     {
+	    	 System.out.println("Player2 Ladder to 23");
+	    	 p2position1=23;
+	     }
+	     else if(p2position1==7)
+	     {
+	    	 System.out.println("Player2 Ladder to 29");
+	    	 p2position1=29;
+	     }
+	     else if(p2position1==22)
+	     {
+	    	 System.out.println("Player2 Ladder to 41");
+	    	 p2position1=41;
+	     }
+	     else if(p2position1==28)
+	     {
+	    	 System.out.println("Player2 Ladder to 77");
+	    	 p2position1=77;
+	     }
+	     else if(p2position1==30)
+	     {
+	    	 System.out.println("Player2 Ladder to 32");
+	    	 p2position1=32;
+	     }
+	     else if(p2position1==54)
+	     {
+	    	 System.out.println("Player2 Ladder to 69");
+	    	 p2position1=77;
+	     }
+	     else if(p2position1==70)
+	     {
+	    	 System.out.println("Player2 Ladder to 90");
+	    	 p2position1=90;
+	     }
+	     else if(p2position1==80)
+	     {
+	    	 System.out.println("Player2 Ladder to 98");
+	    	 p2position1=98;
+	     }
+	     else if(p2position1==27)
+	     {
+	    	 System.out.println("Player2 Snaked to 6");
+	    	 p2position1=6;
+	     }
+	     else if(p2position1==35)
+	     {
+	    	 System.out.println("Player2 Snaked to 5");
+	    	 p2position1=5;
+	     }
+	     else if(p2position1==39)
+	     {
+	    	 System.out.println("Player2 Snaked to 3");
+	    	 p2position1=3;
+	     }
+	     else if(p2position1==50)
+	     {
+	    	 System.out.println("Player2 Snaked to 34");
+	    	 p2position1=34;
+	     }
+	     else if(p2position1==59)
+	     {
+	    	 System.out.println("Player2 Snaked to 46");
+	    	 p2position1=46;
+	     }
+	     else if(p2position1==66)
+	     {
+	    	 System.out.println("Player2 Snaked to 24");
+	    	 p2position1=24;
+	     }
+	     else if(p2position1==73)
+	     {
+	    	 System.out.println("Player2 Snaked to 12");
+	    	 p2position1=12;
+	     }
+	     else if(p2position1==76)
+	     {
+	    	 System.out.println("Player2 Snaked to 63");
+	    	 p2position1=63;
+	     }
+	     else if(p2position1==89)
+	     {
+	    	 System.out.println("Player2 Snaked to 67");
+	    	 p2position1=67;
+	     }
+	     else if(p2position1==97)
+	     {
+	    	 System.out.println("Player2 Snaked to 75");
+	    	 p2position1=75;
+	     }
+	     else if(p2position1==99)
+	     {
+	    	 System.out.println("Player2 Snaked to 26");
+	    	 p2position1=26;
+	     }
+	     
+	     
+	     
+	     
+	     if (p1position1>100 || p2position1>100)
+	     {
+	    	 if(p1position1>100)
+	    	 {
+	    	 p1position1=p1position1-value;
+	    	 continue;
+	    	 }
+	    	 else
+	    	 {
+	    		 p2position1=p2position1-value;
+		    	 continue;
+	    	 }
+	     }else if(p1position1==100 || p2position1==100)
+	     {
+	    	 if(p1position1==100)
+	    	 {
+	    		 System.out.println();
+	    	 System.out.println("Player1 reached 100");
+	    	 System.out.println("Player 1 Wins");
+	    	 System.out.println();
+	    	 break;
+	    	 }
+	    	 else
+	    	 {   System.out.println();
+	    		 System.out.println("Player2 reached 100");
+	    		 System.out.println("Player 2 Wins");
+	    		 System.out.println();
+		    	 break; 
+	    	 }
+	     }
+	     System.out.println("Player1 is at "+ p1position1);
+	     System.out.println("Player2 is at "+ p2position1);
+	     System.out.println();
+	     }
+	     System.out.println("Player1 is now at "+ p1position1); 
+	     System.out.println("Player2 is now at "+ p2position1); 
+	     System.out.println();
+	  
+	} 
 }
 
